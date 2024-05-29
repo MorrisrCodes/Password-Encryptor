bcff8e01ec4c50c0f0f2bc14773e0290d1aad831
Morris <morrisrcodes@gmail.com>
2024-05-28 22:47:39 -0400

    completed gui, began encryption rules

--- passencrypt.py
+++ passencrypt.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+def enctrypt(before):
+    passw = ''
+    if len(before) < 10:
+        passw + ('z')
+    elif len(before) >= 10:
+        passw + ('Z')
+    return passw
+
 root = tk.Tk()
 
 root.title("Password Encryptor")
@@ -18,5 +26,7 @@ afterE.grid(row=1, column=1)
 rule = tk.Label(root, text="Password must be at least 10 characters long, include 1 number, 1 uppercase letter, and 1 special character")  
 rule.grid(row=2, column=0, columnspan=4)
 
+sub = tk.Button(root, text="Encrypt", command=lambda: encrypt(beforeE.get()))
+sub.grid(row=3, column=0, columnspan=2)
 
 root.mainloop()
